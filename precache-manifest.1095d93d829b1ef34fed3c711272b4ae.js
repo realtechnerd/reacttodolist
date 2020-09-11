5e839567219445fa08d8c8cda067d1f087710327
realtechnerd <40891074+realtechnerd@users.noreply.github.com>
2020-09-11 18:01:59 -0400

    Updates

---
+++ precache-manifest.1095d93d829b1ef34fed3c711272b4ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6750028df24970d340bbe5734f39ccfc",
+    "url": "/reacttodolist/index.html"
+  },
+  {
+    "revision": "3124efc1e7ab9bdddbd8",
+    "url": "/reacttodolist/static/css/main.866ea391.chunk.css"
+  },
+  {
+    "revision": "fbea652d4f411ed2eed4",
+    "url": "/reacttodolist/static/js/2.aab2d8cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacttodolist/static/js/2.aab2d8cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3124efc1e7ab9bdddbd8",
+    "url": "/reacttodolist/static/js/main.16f3c719.chunk.js"
+  },
+  {
+    "revision": "25052f3892efac266391",
+    "url": "/reacttodolist/static/js/runtime-main.d58f725a.js"
+  }
+]);
