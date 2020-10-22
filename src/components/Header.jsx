991ec73d47cf32b3ddadb88196d922ad69c20ef9
realtechnerd <send.nithil@gmail.com>
2020-10-22 08:37:23 -0400

    Added version number in header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import "./style.css";
+import v from "../../package.json";
 
 function Header() {
     return(
         <div>
             <div className="Header">
-                <h1>TaskBuddy</h1>
+                <h1>TaskBuddy v{v.version}</h1>
             </div>
         </div>
     )
